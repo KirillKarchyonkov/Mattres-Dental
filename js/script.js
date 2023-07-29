238f4d6b00e7ba3d4759c5aae93b4e49f3df0a0b
KirillKarchyonkov <112825082+KirillKarchyonkov@users.noreply.github.com>
2023-07-29 11:47:50 +0300

    Mattres Dental

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+   $('.slider__body').slick({
+      arrows:true,
+      dots:true,
+      slidesToShow: 5,
+      autoplay:true,
+      rows:1,
+      waitForAnimate:false,
+      centerMode:true,
+      focusOnSelect: true,
+      asNavFor:".bigslider__body",
+      responsive: [
+         {
+            breakpoint: 1100,
+            settings: {
+               slidesToShow: 3
+            }
+         },{
+            breakpoint: 850,
+            settings: {
+               slidesToShow: 3
+            }
+
+         }
+      ]
+   });
+   $('.bigslider__body').slick({
+      arrows:false,
+      fade:true,
+      draggable:false,
+      swipe:true,
+      asNavFor:".slider__body"
+
+   });
+   $('.header__burger').click(function(event){
+      $('.header__burger, .header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
+   $('.header__link').click(function(event){
+      $('.header__burger, .header__menu').removeClass('active');
+      $('body').removeClass('lock');
+   });
+});
+
